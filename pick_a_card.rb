3de4a2360692d5a01c42a10369af779c40e2d038
GasGear <manuhinvanya@gmail.com>
2016-08-13 20:28:34 +0300

    Version 2 (En/Ru)

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Валет', 'Дама', 'Король', 'Туз']
-suits = ['Пики', 'Черви', 'Крести', 'Буби']
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] # Card's value
+suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Card's suit
 
-puts "#{values.sample} #{suits.sample}"
+puts "#{values.sample} #{suits.sample}" # Print a random card
